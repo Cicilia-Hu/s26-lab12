2882b391f46d57eddfae9b6871f190d63c82d2c9
胡可嘉 <hukejia@kokayoshimis-MacBook-Air-5.local>
2026-04-15 16:32:59 -0400

    fix library api

--- src/main/java/edu/cmu/Library/LibraryAccount.java
+++ src/main/java/edu/cmu/Library/LibraryAccount.java
@@ -4,18 +4,14 @@ public class LibraryAccount {
     private LibraryService libraryService;
  
     /**
-     * Retrieves an array of checked out books associated with the specified user ID. If the user
-     * has no books checked out, the returned list will be empty. Since multiple households may
-     * share a single account, the user ID is of the form "libraryID:userName".
-     * e.g., "12345:John Doe"
-     *
-     * @param userId the ID of the user whose books are to be retrieved
+     * Retrieves an array of checked out books associated with the specified user ID and the username. If the user
+     * has no books checked out, the returned list will be empty. 
+     * 
+     * @param name the name of the user who borrow the book
+     * @param id the ID of the user whose books are to be retrieved
      * @return an array of Book objects the user has checked out
      */
-    public Book[] getBooks(String userId) {
-        String[] parts = userId.split(":");
-        String name = parts[0];
-        String id = parts[1];
+    public Book[] getBooks(String name, String id) {
         return libraryService.getBooks(name, id);        
     }
 }
