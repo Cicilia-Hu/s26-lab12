503ad1b3792bcb99ef84d1e7df9122fce182edc1
胡可嘉 <hukejia@kokayoshimis-MacBook-Air-5.local>
2026-04-15 16:43:22 -0400

    fix weather api

--- src/main/java/edu/cmu/Weather/Weather.java
+++ src/main/java/edu/cmu/Weather/Weather.java
@@ -1,27 +1,36 @@
 package edu.cmu.Weather;
 
+
+
 public class Weather {
     private WeatherService weatherService;
-    private boolean inches;
+    private LengthScale scale;
+
+    public enum LengthScale {
+        INCHES,
+        MILLIMETERS
+    }   
 
     /**
      * Sets the length scale for rainfall.
      *
-     * @param inches if true, sets the scale to inches; if false, sets the scale to millimeters.
+     * @param the length scale wanted
      */
-    public void setLengthScale(boolean inches) {
-        this.inches = inches;
+    public void setLengthScale(LengthScale scale) {
+        this.scale = scale;
     }
 
     /**
      * Retrieves the rainfall measurement over the last 24 hours from the weather service in the preferred scale.
      * 
-     * @return the rainfall amount. If the measurement is in inches, it returns the value as is.
-     *         If the measurement is not in inches, it converts the value to millimeters.
+     * @return the rainfall amount in the selected length scale.
+     * The WeatherService provides rainfall in millimeters.
+     * If the preferred scale is inches, the value is converted from millimeters.
+     *         
      */
     public double getRainfall() {
         double wsRainfall = weatherService.getRainfall();
-        if (inches) {
+        if (scale == LengthScale.INCHES) {
             return wsRainfall / 25.4;
         } else {
             return wsRainfall;
